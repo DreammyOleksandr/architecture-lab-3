6d79a2dbd81f5f57cc001fc8c9f9f83d62fd90b3
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-04-24 11:01:27 +0300

    Blue cross display added

--- ui/window.go
+++ ui/window.go
@@ -32,8 +32,8 @@ type Visualizer struct {
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	pw.pos.Max.X = 800
+	pw.pos.Max.Y = 800
 	driver.Main(pw.run)
 }
 
@@ -119,11 +119,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 		}
 
 	case paint.Event:
-		// Малювання контенту вікна.
 		if t == nil {
 			pw.drawDefaultUI()
 		} else {
-			// Використання текстури отриманої через виклик Update.
 			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
 		}
 		pw.w.Publish()
@@ -133,9 +131,20 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	crossSize := 400
+	crossX := (pw.sz.Bounds().Max.X - pw.sz.Bounds().Min.X - crossSize) / 2
+	crossY := (pw.sz.Bounds().Max.Y - pw.sz.Bounds().Min.Y - crossSize) / 2
 
-	// Малювання білої рамки.
+	pw.w.Fill(
+		image.Rect(crossX+crossSize/2-70, crossY, crossX+crossSize/2+70, crossY+crossSize),
+		color.RGBA{B: 255, A: 255},
+		draw.Src,
+	)
+	pw.w.Fill(
+		image.Rect(crossX, crossY+crossSize/2-70, crossX+crossSize, crossY+crossSize/2+70),
+		color.RGBA{B: 255, A: 255},
+		draw.Src)
+
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
 		pw.w.Fill(br, color.White, draw.Src)
 	}
