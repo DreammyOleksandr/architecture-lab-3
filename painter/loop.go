e03caa05ee2b7fcc92c3ab518219312c2e26b193
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-04-24 13:35:52 +0300

    Event loop implementation

--- painter/loop.go
+++ painter/loop.go
@@ -1,9 +1,8 @@
 package painter
 
 import (
-	"image"
-
 	"golang.org/x/exp/shiny/screen"
+	"image"
 )
 
 // Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циклі подій.
@@ -32,18 +31,31 @@ func (l *Loop) Start(s screen.Screen) {
 	l.prev, _ = s.NewTexture(size)
 
 	// TODO: стартувати цикл подій.
+
+	l.stop = make(chan struct{})
+	go func() {
+		for !l.stopReq || l.mq.empty() {
+			op := l.mq.pull()
+			if update := op.Do(l.next); update {
+				l.Receiver.Update(l.next)
+				l.next, l.prev = l.prev, l.next
+			}
+		}
+		close(l.stop)
+	}()
 }
 
 // Post додає нову операцію у внутрішню чергу.
 func (l *Loop) Post(op Operation) {
-	if update := op.Do(l.next); update {
-		l.Receiver.Update(l.next)
-		l.next, l.prev = l.prev, l.next
-	}
+	l.mq.push(op)
 }
 
 // StopAndWait сигналізує про необхідність завершити цикл та блокується до моменту його повної зупинки.
 func (l *Loop) StopAndWait() {
+	l.Post(OperationFunc(func(screen.Texture) {
+		l.stopReq = true
+	}))
+	<-l.stop
 }
 
 // TODO: Реалізувати чергу подій.
@@ -51,10 +63,6 @@ type messageQueue struct{}
 
 func (mq *messageQueue) push(op Operation) {}
 
-func (mq *messageQueue) pull() Operation {
-	return nil
-}
+func (mq *messageQueue) pull() Operation { return nil }
 
-func (mq *messageQueue) empty() bool {
-	return false
-}
+func (mq *messageQueue) empty() bool { return false }
